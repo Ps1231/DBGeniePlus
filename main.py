676a0a81d7d995485505e06bf2fe5ce3d323043b
Priyanshi Soni <103135533+Ps1231@users.noreply.github.com>
2023-10-17 16:30:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+# Function to create corresponding database and tables
 from DatabaseCreator import create_mysql_tables_from_yaml
-from ArgumentParser import parse_arguments
+import argparse
+
+# Function to take yaml file as argument(input) in terminal to ru the program
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Generate fake data and populate MySQL tables based on a YAML configuration file.")
+    parser.add_argument(
+        "config_file", help="Path to the YAML configuration file")
+
+    args = parser.parse_args()
+    return args.config_file
+
+
 config_file_path = parse_arguments()
+# Function to create corresponding database and tables
 create_mysql_tables_from_yaml(config_file_path)
