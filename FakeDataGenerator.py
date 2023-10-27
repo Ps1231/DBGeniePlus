05255caf550700b790ebb25ab24a8dd9dec0110f
Priyanshi Soni <103135533+Ps1231@users.noreply.github.com>
2023-10-27 18:08:28 +0530

    Update FakeDataGenerator.py

--- FakeDataGenerator.py
+++ FakeDataGenerator.py
@@ -20,16 +20,16 @@ def generate_regular_fake_data(column_name, column_info):
         return fake.boolean()
     elif 'DATE' in data_type:
         return fake.date_between(start_date='-1y', end_date='today')
+    elif 'TIMESTAMP' in data_type:
+        return fake.date_time_this_year()
     elif 'TIME' in data_type:
         return fake.time(pattern='%H:%M:%S')
     elif 'DATETIME' in data_type:
         return fake.date_time_this_year()
-    elif 'AGE' in column_name.lower():
+    elif 'AGE' in column_name.upper():
         return fake.random_number(2)
-    elif 'VARCHAR' in data_type:
-        return generate_varchar_data(size)
-    elif 'CHAR' in data_type:
-        return generate_char_data(size)
+    elif 'URL' in column_name.upper() or 'PATH' in column_name.upper():
+        return generate_url_data(size)
     elif 'BIGINT' in data_type:
         return generate_bigint_data(size)
     elif 'INT' in data_type:
@@ -38,7 +38,7 @@ def generate_regular_fake_data(column_name, column_info):
         return generate_text_data(size)
     elif 'CITY' in column_name.upper():
         return generate_city_data(size)
-    elif 'COLOR' in column_name.lower() or 'COLOUR' in column_name.lower():
+    elif 'COLOR' in column_name.upper() or 'COLOUR' in column_name.upper():
         return generate_color_data(size)
     elif 'GENDER' in column_name.lower():
         return generate_gender_data(size)
@@ -54,14 +54,25 @@ def generate_regular_fake_data(column_name, column_info):
         return generate_first_name_data(size)
     elif 'LAST_NAME' in column_name.upper():
         return generate_last_name_data(size)
-    elif 'NAME' in column_name.upper():
+    elif 'NAME' in column_name.upper() or 'TITLE' in column_name.upper():
         return generate_name_data(size)
     elif 'ADDRESS' in column_name.upper():
         return generate_address_data(size)
     elif any(word in column_name.upper() for word in ['PHONE', 'CONTACT', 'MOBILE']):
         return fake.random_number(10)
+    elif 'VARCHAR' in data_type:
+        return generate_varchar_data(size)
+    elif 'CHAR' in data_type:
+        return generate_char_data(size)
 
-# Function for inserting enum values
+
+def generate_url_data(size):
+    if size is not None:
+        return fake.url(schemes=['http', 'https'])
+    else:
+        return fake.url(schemes=['http', 'https'])
+
+
 def generate_enum_data(column_info):
     enum_values_with_quotes = column_info.split('(')[1].split(')')[
         0].split(',')
@@ -70,7 +81,7 @@ def generate_enum_data(column_info):
     random_enum_value = fake.random_element(elements=enum_values)
     return random_enum_value.strip("'")
 
-# Function for inserting Varchar values
+
 def generate_varchar_data(size):
     if size is not None and size > 50:
         return fake.text(max_nb_chars=size)
@@ -79,7 +90,7 @@ def generate_varchar_data(size):
     else:
         return random.choice(string.ascii_uppercase)
 
-# Function for inserting values of char DataType
+
 def generate_char_data(size):
     if size is not None and size > 50:
         return fake.text(max_nb_chars=size)
@@ -88,34 +99,34 @@ def generate_char_data(size):
     else:
         return random.choice(string.ascii_uppercase)
 
-# Function for inserting values of bigint DataType
+
 def generate_bigint_data(size):
     if size is not None:
         return fake.random_int(min=1, max=10 ** size - 1)
     else:
         return fake.random_number(10)
 
-# Function for inserting values of int DataType
+
 def generate_int_data(size):
     if size is not None:
         return fake.random_int(min=1, max=10 ** size - 1)
     else:
         return fake.random_int(min=1, max=99999)
 
-# Function for inserting values of decimal DataType
+
 def generate_decimal_data(column_info):
     parts = column_info.split('(')[1].split(')')[0].split(',')
     precision = int(parts[0])
     scale = int(parts[1]) if len(parts) > 1 else 2
     return abs(fake.pydecimal(left_digits=precision - scale, right_digits=scale))
 
-# Function for inserting fake text of given size
+
 def generate_text_data(size):
     if size is not None:
         return fake.text(max_nb_chars=size)
     return fake.text(max_nb_chars=200)
 
-# Function for inserting name of random cities
+
 def generate_city_data(size):
     if size is not None and size <= 2:
         return fake.city_suffix()
@@ -125,22 +136,21 @@ def generate_city_data(size):
         return fake.text(max_nb_chars=size)
     return fake.city()
 
-# Function for inserting random colors
+
 def generate_color_data(size):
     fake_color = fake.color_name()
     if len(fake_color) > size:
         fake_color = fake_color[:size]
     return fake_color
 
-# Function for inserting random gender
-# you can modify according to your convienience
+
 def generate_gender_data(size):
     if size > 5:
         return fake.random_element(elements=('male', 'female'))
     else:
         return fake.random_element(elements=('M', 'F'))
 
-# Function for inserting random names for state
+
 def generate_state_data(size):
     if size is not None and size >= 3:
         return fake.state_abbr()
@@ -148,49 +158,49 @@ def generate_state_data(size):
         return fake.text(max_nb_chars=size)
     return fake.state_abbr()
 
-# Function for inserting random emails
+
 def generate_email_data(size):
     email = fake.email()
     if len(email) > size:
         email = email[:size]
     return email
 
-# Function for inserting random countries
+
 def generate_country_data(size):
     fake_country = fake.country()
     if len(fake_country) > size:
         fake_country = fake_country[:size]
     return fake_country
 
-# Function for inserting random passwords
+
 def generate_password_data(size):
     if size is not None:
         return fake.password(length=size)
     else:
         return fake.password()
 
-# Function for inserting random values of first name
+
 def generate_first_name_data(size):
     first_name = fake.first_name()
     if len(first_name) > size:
         first_name = first_name[:size]
     return first_name
 
-# Function for inserting random values of last name
+
 def generate_last_name_data(size):
     last_name = fake.last_name()
     if len(last_name) > size:
         last_name = last_name[:size]
     return last_name
 
-# Function for inserting random values of name
+
 def generate_name_data(size):
     name = fake.name()
     if len(name) > size:
         name = name[:size]
     return name
 
-# Function for inserting random addresses
+
 def generate_address_data(size):
     fake_add = fake.address()
     if len(fake_add) > size:
